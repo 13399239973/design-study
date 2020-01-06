c0f96e261de54018ce5c40414b0eb54be330b7b0
马娟娟 <2640700551@qq.com>
2020-01-06 14:46:25 +0800

    添加配置中心，网关相关的配置

--- design-auth/src/main/java/com/design/auth/controller/LoginController.java
+++ design-auth/src/main/java/com/design/auth/controller/LoginController.java
@@ -10,14 +10,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/auth")
 public class LoginController {
-    @Autowired
-    UserService userService;
+
 
     @RequestMapping(value = "/findByUserName",method = RequestMethod.POST)
     public User findByUserName(@RequestParam("username") String username,@RequestParam("password") String password){
 
         return null;
+    }
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String login() {
+        String result="{\"code\":200,\"data\":\"success\"}";
+        return result;
     }
 }
